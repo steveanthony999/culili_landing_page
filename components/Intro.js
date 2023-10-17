9d74d75bcf5b33b07472686ab9d56c057cfdf2d1
Steven Woodward <steveanthony999@gmail.com>
2023-10-16 20:41:42 -0700

    Added blob and styling, animation

--- components/Intro.js
+++ components/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Blob from '../components/Blob';
 import styles from './Intro.module.css';
 
 function Intro({ onSelection }) {
@@ -18,11 +19,17 @@ function Intro({ onSelection }) {
           exit={{ opacity: 0 }}
           className={styles.imageCard}
         >
-          <img
+          <Blob dur={8} />
+          <motion.img
             src="/images/cat.png"
             alt="Cat Person"
             onClick={() => handleSelection('cat')}
             className={styles.image}
+            style={{
+              position: 'absolute',
+              top: 25,
+              left: 35,
+            }}
           />
           <div className={styles.label}>Cat Person</div>
         </motion.div>
@@ -32,11 +39,17 @@ function Intro({ onSelection }) {
           exit={{ opacity: 0 }}
           className={styles.imageCard}
         >
-          <img
+          <Blob dur={12} flipXY />
+          <motion.img
             src="/images/dog.png"
             alt="Dog Person"
             onClick={() => handleSelection('dog')}
             className={styles.image}
+            style={{
+              position: 'absolute',
+              top: 25,
+              left: 75,
+            }}
           />
           <div className={styles.label}>Dog Person</div>
         </motion.div>
@@ -46,11 +59,17 @@ function Intro({ onSelection }) {
           exit={{ opacity: 0 }}
           className={styles.imageCard}
         >
-          <img
+          <Blob dur={4} flipX />
+          <motion.img
             src="/images/penguin.png"
             alt="Penguin Person"
             onClick={() => handleSelection('penguin')}
             className={`${styles.image} ${styles.penguinImage}`}
+            style={{
+              position: 'absolute',
+              top: 25,
+              left: 95,
+            }}
           />
           <div className={styles.label}>Penguin Person</div>
         </motion.div>
