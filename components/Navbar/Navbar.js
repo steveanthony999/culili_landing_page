6c31996e14e12472acf365cd00fc0868dca768c0
Steven Woodward <steveanthony999@gmail.com>
2023-10-20 16:47:41 -0700

    Added scrolling styling to navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import common from '@/styles/common.module.css';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.Navbar__left}>
         <Link className={common.logo} href="/">
           Culili
