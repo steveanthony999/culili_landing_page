788e6febe5c10502e627daaaf035d52fae0ff1a0
Steven Woodward <steveanthony999@gmail.com>
2023-11-01 23:10:54 -0700

    Added terms of service

---
+++ pages/terms-of-service.js
@@ -0,0 +1,400 @@
+import Layout from '@/components/Layout';
+import styles from '@/styles/policies.module.css';
+
+const TermsOfService = () => {
+  return (
+    <Layout>
+      <div className={styles.Policies}>
+        <div className={styles.container}>
+          <h1>Terms of Service</h1>
+
+          {/* Introduction */}
+          <section>
+            <h2>Introduction</h2>
+            <p>
+              Welcome to Culili, a platform conceived and operated by Steven
+              Woodward, aimed at empowering freelance frontend developers by
+              providing a fully-functional backend platform. This enables
+              developers to offer full-stack web development services to small
+              restaurant owners, thereby simplifying the process of creating
+              robust online presences for these businesses.
+            </p>
+            <p>
+              By accessing or using Culili, you are entering into a legal
+              agreement with Steven Woodward, the sole proprietor behind Culili,
+              and agree to be bound by these Terms of Service. It's imperative
+              that you read and understand these terms thoroughly as they govern
+              your use of Culili and the services provided herein.
+            </p>
+            <p>
+              Your use of Culili is conditional upon your acceptance of these
+              Terms of Service and your compliance with any applicable laws and
+              regulations. Users must be of legal age to conduct business to use
+              our platform and avail our services. The platform and dashboard of
+              Culili are accessible to users as long as their subscription
+              remains active and current.
+            </p>
+            <p>
+              If you have any questions or concerns regarding these terms, feel
+              free to contact us.
+            </p>
+          </section>
+
+          {/* Use of Service */}
+          <section>
+            <h2>Use of Service</h2>
+            <p>
+              Culili is designed to provide frontend developers with a robust
+              backend platform, facilitating the provision of full-stack web
+              development services to small, non-franchise restaurant owners.
+              While users have the liberty to utilize the platform beyond this
+              scope, our primary focus remains on aiding the aforementioned
+              demographic.
+            </p>
+            <ul>
+              <li>
+                <span>API Keys</span>: To utilize the full spectrum of services
+                offered by Culili, users are required to provide API keys for
+                certain third-party services as specified by us. Currently, this
+                includes Stripe and Google Maps. The list of required
+                third-party services may expand in the future, and users are
+                obliged to provide accurate and valid API keys to continue using
+                our platform.
+              </li>
+              <li>
+                <span>Acceptable Use</span>: Users are encouraged to use the
+                platform in a manner consistent with its intended purpose. While
+                there's flexibility in how the platform can be used, we urge
+                users to adhere to the core objective of aiding small,
+                non-franchise restaurants.
+              </li>
+              <li>
+                <span>Unacceptable Use</span>: Any actions that violate
+                applicable laws, infringe upon the rights of others, or
+                interfere with the normal functioning of Culili are deemed
+                unacceptable. This includes, but is not limited to, unauthorized
+                access to other user accounts, distribution of malicious
+                software, or engaging in fraudulent activities.
+              </li>
+              <li>
+                <span>Behavior Guidelines</span>: We encourage a professional
+                and respectful interaction among our user community. Any actions
+                that foster a positive and collaborative environment are
+                appreciated, while behaviors that detract from such an
+                atmosphere are discouraged.
+              </li>
+            </ul>
+            <p>
+              Culili reserves the right to take appropriate action, including
+              suspension or termination of user accounts, in cases of violation
+              of these usage terms.
+            </p>
+          </section>
+
+          {/* User Accounts and Responsibilities */}
+          <section>
+            <h2>User Accounts and Responsibilities</h2>
+            <p>
+              Creating an account on Culili requires minimal information but is
+              a crucial step towards accessing the services provided by our
+              platform. Here are the key points regarding user accounts and
+              responsibilities:
+            </p>
+            <ul>
+              <li>
+                <span>Account Creation</span>: To create an account on Culili,
+                users are required to provide their first name and email
+                address.
+              </li>
+              <li>
+                <span>Verification</span>: Upon registration, an email
+                verification process is initiated to ensure the authenticity of
+                the user. Additionally, users may need to undergo verification
+                processes required by our third-party API providers to fully
+                utilize our platform.
+              </li>
+              <li>
+                <span>Account Security</span>: Users are responsible for
+                maintaining the confidentiality of their account credentials,
+                including their password. It's essential to keep your account
+                secure to prevent unauthorized access and ensure a safe user
+                experience on Culili.
+              </li>
+              <li>
+                <span>Unique Accounts</span>: Each user is allowed to have one
+                account on Culili as every email address used for registration
+                must be unique. Our subscription plans are structured in such a
+                way that having multiple accounts does not provide any
+                additional benefit to the user.
+              </li>
+              <li>
+                <span>Unauthorized Access</span>: In the event of any suspected
+                unauthorized access to your account, it's crucial to contact us
+                immediately. Prompt reporting can aid in ensuring the security
+                of your account and mitigate any potential issues.
+              </li>
+              <p>
+                Culili is dedicated to providing a secure and straightforward
+                user experience, and adherence to these guidelines helps in
+                maintaining a trustworthy environment for all users.
+              </p>
+            </ul>
+          </section>
+
+          {/* Intellectual Property */}
+          <section>
+            <h2>Intellectual Property</h2>
+            <p>
+              The intellectual property rights concerning the content created or
+              shared on Culili are defined as follows:
+            </p>
+            <ul>
+              <li>
+                <span>Ownership</span>:
+                <ul>
+                  <li>
+                    <span>Content created by us</span>: All content created by
+                    us on Culili remains our exclusive property.
+                  </li>
+                  <li>
+                    <span>User Content</span>: Users retain full ownership of
+                    the content they create or share on Culili.
+                  </li>
+                  <li>
+                    <span>Guest Blog Posts</span>: In instances where users are
+                    invited to create blog posts on Culili, joint ownership is
+                    established between the user and us. However, we reserve the
+                    right to remove such content if deemed necessary. Users are
+                    free to use their blog posts elsewhere provided they include
+                    a backlink to the original post on Culili. Pre-existing blog
+                    posts created before being posted on Culili are not
+                    permitted.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <span>Transfer of Rights</span>: There is no transfer of
+                intellectual property rights upon the completion of a project on
+                Culili.
+              </li>
+              <li>
+                <span>Copyright Infringement</span>: We take claims of copyright
+                infringement seriously. If content is found to be infringing, it
+                will be removed, and the concerned user will be contacted to
+                resolve the dispute.
+              </li>
+              <li>
+                <span>Third-Party Content</span>: Culili utilizes third-party
+                content based on licensing agreements, such as stock graphics
+                and AI-generated art. The use and display of such content are
+                governed by the respective licensing terms.
+              </li>
+            </ul>
+            <p>
+              We encourage our users to respect the intellectual property rights
+              of others while using our platform and to be aware of the
+              ownership and rights associated with the content they create or
+              share on Culili.
+            </p>
+          </section>
+
+          {/* Disclaimers and Limitations of Liability */}
+          <section>
+            <h2>Disclaimers and Limitations of Liability</h2>
+            <ul>
+              <li>
+                <span>Disclaimers</span>:
+                <ul>
+                  <li>
+                    The services provided by Culili are intended to facilitate
+                    connections between freelance frontend developers and small
+                    restaurant owners, alongside offering a robust backend
+                    platform for website development. However, the results,
+                    including the satisfaction of restaurant owners or the
+                    success of projects, are not guaranteed by Culili.
+                  </li>
+                  <li>
+                    While we strive to maintain a high level of service, the
+                    operation of Culili may be subject to factors beyond our
+                    control, and we do not guarantee uninterrupted or error-free
+                    service.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <span>Limitation of Liability</span>: To the fullest extent
+                permitted by applicable law, Culili, along with its owner Steven
+                Woodward, shall not be liable for any direct, indirect,
+                incidental, special, consequential, or exemplary damages,
+                including but not limited to, damages for loss of profits,
+                goodwill, use, data, or other intangible losses resulting from
+                the use of or inability to use the service provided by Culili.
+              </li>
+              <li>
+                <span>No Liability for Third Party Services</span>: Culili
+                integrates with third-party services such as Stripe and Google
+                Maps. We are not liable for any damages or issues arising from
+                the use of these third-party services. Users are advised to read
+                and understand the terms and privacy policies of these
+                third-party services before using them.
+              </li>
+              <li>
+                <span>Accuracy of Information</span>: We endeavor to maintain
+                accurate and reliable information on Culili. However, we do not
+                warrant the completeness, accuracy, or reliability of the
+                information provided. Users are responsible for verifying any
+                information before relying on it.
+              </li>
+              <p>
+                By using Culili, you acknowledge and agree to these disclaimers
+                and limitations of liability.
+              </p>
+            </ul>
+          </section>
+
+          {/* Termination */}
+          <section>
+            <h2>Termination</h2>
+            <p>
+              Culili values the relationship with its users but under certain
+              circumstances, it may be necessary to terminate user accounts. The
+              circumstances under which termination may occur include, but are
+              not limited to:
+            </p>
+            <ul>
+              <li>
+                <span>Non-Payment</span>: Failure to pay the subscription fees
+                as required to maintain access to Culili's services.
+              </li>
+              <li>
+                <span>User-Led Termination</span>: Users have the right to
+                terminate their account at any time.
+              </li>
+              <li>
+                <span>Violation of Terms</span>: Any violation of these Terms of
+                Service or other policies set forth by Culili.
+              </li>
+              <li>
+                <span>Unlawful Activity</span>: Engaging in activities that are
+                unlawful or promoting unlawful activities.
+              </li>
+            </ul>
+            <p>Upon termination, the following applies:</p>
+            <ul>
+              <li>
+                <span>Data Deletion</span>: User data will be deleted 60 days
+                post-termination. Notifications regarding data deletion will be
+                sent to users as outlined in our Privacy Policy.
+              </li>
+              <li>
+                <span>Appeal Process</span>: Users may appeal termination or
+                seek to recover their account by contacting us.
+              </li>
+              <li>
+                <span>Notification</span>: Users will be notified via email in
+                the event of account termination initiated by Culili.
+              </li>
+            </ul>
+            <p>
+              We reserve the right to terminate user accounts at our sole
+              discretion, while also providing a pathway for communication and
+              resolution wherever possible.
+            </p>
+          </section>
+
+          {/* Governing Law */}
+          <section>
+            <h2>Governing Law</h2>
+            <ul>
+              <li>
+                <span>Jurisdiction</span>: These Terms of Service and any
+                disputes arising out of or related to the use of Culili shall be
+                governed by the laws of the United States.
+              </li>
+              <li>
+                <span>Compliance</span>: Users are responsible for complying
+                with all local, state, and federal laws applicable to their use
+                of Culili. Culili adheres to the legal standards and regulations
+                as stipulated by the laws of the United States.
+              </li>
+              <li>
+                <span>Dispute Resolution</span>: In the event of a dispute, we
+                encourage users to contact us directly to seek a resolution. We
+                are open to engaging in a dialogue to address any concerns.
+                However, if a resolution cannot be reached, legal proceedings
+                may be initiated, and users agree to resolve any disputes
+                through the legal system as governed by United States law.
+              </li>
+            </ul>
+            <p>
+              We are committed to adhering to the highest legal and ethical
+              standards in the operation of Culili and ensuring a fair and
+              respectful environment for all users.
+            </p>
+          </section>
+
+          {/* Changes to the Terms */}
+          <section>
+            <h2>Changes to the Terms</h2>
+            <p>
+              Culili reserves the right to modify or update these Terms of
+              Service at any time to reflect changes in our practices, service
+              offerings, or legal or regulatory requirements. Here is how we
+              will handle changes to these terms:
+            </p>
+            <ul>
+              <li>
+                <span>Notification</span>: Users will be notified of any changes
+                to the Terms of Service via email. The email will provide a
+                summary of the key changes and a link to the updated Terms of
+                Service.
+              </li>
+              <li>
+                <span>Acceptance</span>: To continue using Culili, users will
+                need to agree to the updated Terms of Service. If a user does
+                not agree to the updated terms, they should stop using the
+                service.
+              </li>
+              <li>
+                <span>Effective Date</span>: Changes to the Terms of Service
+                will become effective 30 days after the notification is sent.
+                This provides users with a reasonable timeframe to review the
+                updated terms and decide whether or not they wish to continue
+                using Culili under the new terms.
+              </li>
+            </ul>
+            <p>
+              We encourage users to periodically review the Terms of Service to
+              stay informed on any updates.
+            </p>
+          </section>
+
+          {/* Contact Information */}
+          <section>
+            <h2>Contact Information</h2>
+            <p>
+              For any legal inquiries or concerns regarding these Terms of
+              Service, users are encouraged to contact us through the following
+              means:
+            </p>
+            <ul>
+              <li>
+                <span>Email</span>: legal@culili.com
+              </li>
+              <li>
+                <span>Contact Form</span>: Accessible on our website under the
+                "Contact Us" section.
+              </li>
+            </ul>
+            <p>
+              We are committed to addressing your inquiries and concerns in a
+              timely and professional manner.
+            </p>
+          </section>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default TermsOfService;
